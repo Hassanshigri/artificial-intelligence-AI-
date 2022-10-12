614f846ccccc1aa1588cda5416b0955322e5e497
Muhammad Hassan <96192977+Hassanshigri@users.noreply.github.com>
2022-10-12 12:02:34 +0500

    Add files via upload

---
+++ artificial intelligence (AI)/course content/first practicle/Hellow world.py
@@ -0,0 +1,10 @@
+#In our first program we will simply print Hello World.
+#We have to use print keyword to display any text or massage
+#After the keyword use parentheses and type anything inside single qutation or double ('',"") to display.
+
+#eg:
+print('Hello, world!') 
+#eg:
+print("i am Hassu")
+#eg:
+print(2+3)
